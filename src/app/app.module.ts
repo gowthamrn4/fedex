ac6b4c755446d81766a0751cf0d6ddd3b33eb37a
gowthamrn4 <gowthamrn4@gmail.com>
2018-10-25 15:09:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
+import {HomeComponent} from './home/home.component';
+import { from } from 'rxjs';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path:'',pathMatch:'full',redirectTo:'home' },
+      { path:'home',component:HomeComponent},
+     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
